2f6f517aa729f2ae7ac633e33ecb3befef284c8d
adisornnakyim2@gmail.com <beer0649894255>
2020-05-22 01:43:09 +0700

    final commit

---
+++ configfirebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYc_FGMgZ0e_HDDicus0_zzsEhSn6ZnFM",
+    authDomain: "test-36615.firebaseapp.com",
+    databaseURL: "https://test-36615.firebaseio.com",
+    projectId: "test-36615",
+    storageBucket: "test-36615.appspot.com",
+    messagingSenderId: "468576715747",
+    appId: "1:468576715747:web:489c1d3414c67948ea96d6",
+    measurementId: "G-EVPD32VM7W"
+};// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
